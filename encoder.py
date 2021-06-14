2da1700357aa371328a8a495d2c3244a3c2d593c
FEDERICA <phaedria84@yahoo.it>
2021-06-14 20:09:37 +0200

    initial commit

---
+++ encoder.py
@@ -0,0 +1,31 @@
+
+from the_code import *
+
+def userinput():
+    action = input('Would you like to Encode or Decode a message? ').lower() 
+    if action == 'encode':
+        message = input ('Input your message: ').lower()
+        encoding(message)
+        more()
+
+
+    elif action == 'decode':
+        message = input ('Input your message: ').lower()
+        decoding(message)
+        more()
+
+    else:
+        print ('Wrong input. Try again')
+        userinput()
+ 
+def more():
+    goagain = input('Encode/Decode more? ').lower()
+    if goagain == 'yes':
+        userinput()
+    elif goagain == 'no':
+        print('Ok. Bye')
+    else:
+        print ('Wrong input. Try again')
+        more()
+
+userinput()
